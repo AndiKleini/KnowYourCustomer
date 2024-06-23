76df006f0ac01d0fa3b50dbc9bb081179e7f1072
Andreas Kleinbichler <Andi.Kleinbichler@gmail.com>
2024-06-23 12:18:04 +0200

    feature: add test for retrieving no requested customer data

--- KycAppCoreTests/LoyaltyProfileTests.cs
+++ KycAppCoreTests/LoyaltyProfileTests.cs
@@ -15,11 +15,10 @@ public class LoyaltyProfileTests
         int customerSignedUpYesterday = 2;
         var activityStoreMock = new Moq.Mock<ICustomerActivityStore>();
         activityStoreMock.Setup(s => s.GetEventsFor(customerSignedUpMoreThan365DaysAgo)).Returns(
-            new List<CustomerActivityEventBase>() 
-            {
+            [
                 new SignUpActivityEvent(customerSignedUpYesterday, DateTime.Now.AddDays(-1)),
                 new SignUpActivityEvent(customerSignedUpMoreThan365DaysAgo, DateTime.Now.AddDays(-500))
-            });
+            ]);
         var instanceUnderTest = new LoyaltyProfile(activityStoreMock.Object);
 
         instanceUnderTest.GenerateProfile(customerSignedUpMoreThan365DaysAgo);
@@ -28,11 +27,28 @@ public class LoyaltyProfileTests
     }
     
     [Test]
-    public void GenerateProfile_NoSignUpEventRetrieved_SwitchToErrorState()
+    public void GenerateProfile_NoSignUpEventRetrievedAtall_SwitchToErrorStateUnknownCustomer()
     {
         int customerId = 1;
         var activityStoreMock = new Moq.Mock<ICustomerActivityStore>();
         activityStoreMock.Setup(s => s.GetEventsFor(customerId)).Returns([]);
+        var instanceUnderTest = new LoyaltyProfile(activityStoreMock.Object);
+
+        instanceUnderTest.GenerateProfile(customerId);
+
+        instanceUnderTest.Points.Should().Be(0);
+        instanceUnderTest.Error.Should().Be(ErrorCodes.UnknownCustomer);
+    }
+    
+    [Test]
+    public void GenerateProfile_NoSignUpEventRetrievedForSpecifiedCustomer_SwitchToErrorStateUnknownCustomer()
+    {
+        int customerId = 1;
+        int anotherCustomerId = 2;
+        var activityStoreMock = new Moq.Mock<ICustomerActivityStore>();
+        activityStoreMock.Setup(s => s.GetEventsFor(customerId)).Returns([
+            new SignUpActivityEvent(anotherCustomerId, DateTime.Now.AddDays(-1))
+        ]);
         var instanceUnderTest = new LoyaltyProfile(activityStoreMock.Object);
 
         instanceUnderTest.GenerateProfile(customerId);
