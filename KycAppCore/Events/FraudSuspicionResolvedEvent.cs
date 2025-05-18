d96cb1e60b515703d612579e84d594e24c8c435b
AndiKleini <andreaskleinbichler@Andreass-MacBook-Pro.local>
2025-05-18 20:53:11 +0200

    fix, fraudSuspiciosId has to be an int

--- KycAppCore/Events/FraudSuspicionResolvedEvent.cs
+++ KycAppCore/Events/FraudSuspicionResolvedEvent.cs
@@ -1,4 +1,4 @@
 namespace KycAppCore.Events;
 
-public record FraudSuspicionResolvedEvent(int CustomerUnderTestId, DateTime activitTimeStamp, object FraudSuspicionId) : 
+public record FraudSuspicionResolvedEvent(int CustomerUnderTestId, DateTime activitTimeStamp, int FraudSuspicionId) : 
     CustomerActivityEventBase(CustomerUnderTestId, activitTimeStamp);
